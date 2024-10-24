63053fa86d215e4184e4357d902ea8a0a3f36d72
Aynurmm <aynurmammadova09@gmail.com>
2024-10-24 20:34:28 +0400

    print hello world

--- src/main/java/ders1/Main.java
+++ src/main/java/ders1/Main.java
@@ -2,6 +2,19 @@ package ders1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.print("Hello, world!");
+//        System.out.println("Hello, world");
+//        System.out.printf("Hello, %s", "World");
+//        System.out.printf("Hello, World: %.2f\n", 50.0656);
+
+//        for (int i=1;i<6;i++){
+//            System.out.println(i);
+//        }
+//        System.out.println(1);
+//        System.out.println(2);
+//        System.out.println(3);
+//        System.out.println(4);
+//        System.out.println(5);
+        System.out.println("1\n2\n3\n4\n5");
     }
 }
